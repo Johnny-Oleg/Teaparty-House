4533cf4254ff5a1a8475a62a748694c130071540
Johnny-Oleg <neojin.entity@gmail.com>
2020-12-18 01:24:22 +0300

    testing db users

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import './App.css';
+import axios from 'axios';
+
+import data from './database/users.json';
 
 const App = () => {
+  const fetching = () => {
+    console.log(data.users);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={fetching}></button>
+      {data.users.map((item, index) => {
+        return (
+          <li key={item.id}>
+            {item.name}
+          </li>
+        )
+      })}
     </div>
   );
 };
