dc3578fe8d2329d00482a390600110143756a30c
Johnny-Oleg <neojin.entity@gmail.com>
2020-12-18 14:51:50 +0300

    json fix + hooks

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import data from './database/users.json';
+import './App.css';
 
 const App = () => {
+  const [users, setUsers] = useState([]);
+
   const fetching = () => {
     console.log(data.users);
+    setUsers(data.users);
   }
+  
+  console.log(users);
 
   return (
     <div className="App">
       <button onClick={fetching}></button>
-      {data.users.map((item, index) => {
+      {users.map((item, index) => {
         return (
           <li key={item.id}>
             {item.name}
